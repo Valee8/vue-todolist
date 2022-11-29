46d0e27096e97470a972adab2856d8b396c4ca9f
Vale <oirelav95@gmail.com>
2022-11-29 16:39:41 +0100

    aggiungo js

--- js/myscript.js
+++ js/myscript.js
@@ -12,23 +12,42 @@ const {createApp } = Vue;
 
 createApp({
     data() {
-      return {
-        tasks: [
-            { 
-                text: 'Fare i compiti',
-                done: false
-            }, 
+        return {
+            error: false,
+            newTask: [],
+            tasks: [
+                { 
+                    text: 'Fare i compiti',
+                    done: false
+                }, 
 
-            { 
-                text: 'Fare la spesa', 
-                done: true 
-            }, 
-            
-            { 
-                text: 'Fare il bucato', 
-                done: false
+                { 
+                    text: 'Fare la spesa', 
+                    done: true 
+                }, 
+                
+                { 
+                    text: 'Fare il bucato', 
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        addTask() {
+            if (this.newTask.length < 5 || this.newTask === "") {
+                this.error = true;
             }
-        ]
-    }
+            else {
+                this.tasks.unshift({text: this.newTask, done: false});
+                this.error = false;
+            }
+
+            this.newTask = [];
+        },
+
+        deleteTask(indice) {
+            this.tasks.splice(indice, 1);
+        }
 }
 }).mount('#app')
